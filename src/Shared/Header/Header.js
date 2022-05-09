29e3007161ed7c4f8a8d96538c21b76837c9e601
U Sen Wan <senwan96@gmail.com>
2022-05-09 21:04:04 +0600

    top menu added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -4,9 +4,15 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-    <Navbar className='navbar' collapseOnSelect expand="lg" variant="dark">
+    <div>
+        <div className='bg-warning text-white p-1 d-flex justify-content-center align-items-center gap-3'>
+            <small>WEEK DAYS: 05:00 - 22:00</small>
+            <small>SATURDAY: 08:00 - 18:00</small>
+            <small>SUNDAY: CLOSED</small>
+        </div>
+        <Navbar sticky='top' className='navbar' collapseOnSelect expand="lg" variant="dark">
         <div className="container-fluid border-bottom">
-        <Container className='d-flex justify-content-center align-items-center p-2'>
+        <Container className='d-flex justify-content-center align-items-center'>
             <Navbar.Brand className='text-secondary fw-bold fs-3' as={Link} to='/'>Gym Trainer</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
@@ -24,6 +30,7 @@ const Header = () => {
         </Container>
         </div>
     </Navbar>
+    </div>
     );
 };
 
