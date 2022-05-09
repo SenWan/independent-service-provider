9378e6e1206b44b9063a3f894ff64bdb3e386467
U Sen Wan <senwan96@gmail.com>
2022-05-09 20:46:11 +0600

    header added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
+    <Navbar className='navbar' collapseOnSelect expand="lg" variant="dark">
+        <div className="container-fluid border-bottom">
+        <Container className='d-flex justify-content-center align-items-center p-2'>
+            <Navbar.Brand className='text-secondary fw-bold fs-3' as={Link} to='/'>Gym Trainer</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                </Nav>
+                <Nav>
+                <Nav.Link href='home#services' className='text-secondary fw-bold fs-5'>Services</Nav.Link>
+                <Nav.Link className='text-secondary fw-bold fs-5' as={Link} to='blogs'>Blogs</Nav.Link>
+                <Nav.Link className='text-secondary fw-bold fs-5' as={Link} to='about'>About Me</Nav.Link>
+                <Nav.Link className='text-secondary fw-bold fs-5' eventKey={2} href="#memes">
+                    Login
+                </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
         </div>
+    </Navbar>
     );
 };
 
