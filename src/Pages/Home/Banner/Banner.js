d4d48028423e2be5d2a628c2178f79ea1bc0f76c
U Sen Wan <senwan96@gmail.com>
2022-05-09 23:21:52 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Carousel} from 'react-bootstrap';
+import banner1 from '../../../images/banner/pexels-binyamin-mellish-116079.jpg';
+import banner2 from '../../../images/banner/pexels-cesar-galeão-3253501.jpg';
+import banner3 from '../../../images/banner/pexels-pixabay-260447.jpg';
 
 const Banner = () => {
+    const [index, setIndex] = useState(0);
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
     return (
-        <div>
-            <h2>Banner</h2>
-        </div>
+        <Carousel className='mt-3' activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src={banner1}
+                alt="First slide"
+            />
+            <Carousel.Caption>
+                <h3>Ball Handling</h3>
+                <p>It helps Your Arms Stronger</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src={banner2}
+                alt="Second slide"
+            />
+
+            <Carousel.Caption>
+                <h3>Work Hard</h3>
+                <p>There is no shortcut. You have to work hard</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src={banner3}
+                alt="Third slide"
+            />
+
+            <Carousel.Caption>
+                <h3>Boxing</h3>
+                <p>
+                    Boxing services are available here
+                </p>
+            </Carousel.Caption>
+        </Carousel.Item>
+    </Carousel>
     );
 };
 
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -4,25 +4,20 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-    <div>
-        <div className='bg-warning text-white p-1 d-flex justify-content-center align-items-center gap-3'>
-            <small>WEEK DAYS: 05:00 - 22:00</small>
-            <small>SATURDAY: 08:00 - 18:00</small>
-            <small>SUNDAY: CLOSED</small>
-        </div>
+    <>
         <Navbar sticky='top' className='navbar' collapseOnSelect expand="lg" variant="dark">
         <div className="container-fluid border-bottom">
-        <Container className='d-flex justify-content-center align-items-center'>
-            <Navbar.Brand className='text-secondary fw-bold fs-3' as={Link} to='/'>Gym Trainer</Navbar.Brand>
+        <Container className='d-flex justify-content-center align-items-center p-2'>
+            <Navbar.Brand className='text-info fw-bold fs-3' as={Link} to='/'>Gym Trainer</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
                 </Nav>
                 <Nav>
-                <Nav.Link href='home#services' className='text-secondary fw-bold fs-5'>Services</Nav.Link>
-                <Nav.Link className='text-secondary fw-bold fs-5' as={Link} to='blogs'>Blogs</Nav.Link>
-                <Nav.Link className='text-secondary fw-bold fs-5' as={Link} to='about'>About Me</Nav.Link>
-                <Nav.Link className='text-secondary fw-bold fs-5' eventKey={2} href="#memes">
+                <Nav.Link href='home#services' className='text-info fw-bold fs-6'>Services</Nav.Link>
+                <Nav.Link className='text-info fw-bold fs-6' as={Link} to='blogs'>Blogs</Nav.Link>
+                <Nav.Link className='text-info fw-bold fs-6' as={Link} to='about'>About</Nav.Link>
+                <Nav.Link className='text-info fw-bold fs-6' eventKey={2} href="#memes">
                     Login
                 </Nav.Link>
                 </Nav>
@@ -30,7 +25,7 @@ const Header = () => {
         </Container>
         </div>
     </Navbar>
-    </div>
+    </>
     );
 };
 
